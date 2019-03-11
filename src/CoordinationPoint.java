2b49deced9753b9d90146b8ae7b9e06044a45b52
Kalla <kallamcreates@gmail.com>
2019-03-11 21:32:44 +0100

    modified class

--- src/CoordinationPoint.java
+++ src/CoordinationPoint.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class CoordinationPoint {
+    int x;
+    int y;
+
+    public CoordinationPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
 }
